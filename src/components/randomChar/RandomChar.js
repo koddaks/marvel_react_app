36ff09802173f849ab05ce853a7e3372f95659ea
koddaks <koss199710@gmail.com>
2023-04-25 10:41:35 +0300

    componentWillUnmount

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,18 +8,24 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 
 
-class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
+class RandomChar extends Component {  
 
     state = {
         char: {},
         loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
 
     onCharLoaded = (char) => {
         this.setState({
