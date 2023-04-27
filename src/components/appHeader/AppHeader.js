841ab1983d1d67d817adbc8c53d1e49867f988ff
koddaks <koss199710@gmail.com>
2023-04-27 13:30:31 +0300

    add hrefs

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -4,15 +4,15 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <a href="https://www.marvel.com/corporate/advertising">
                     <span>Marvel</span> information portal
                 </a>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><a href="https://www.marvel.com/characters">Characters</a></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><a href="https://www.marvel.com/comics?&options%5Boffset%5D=0&totalcount=12">Comics</a></li>
                 </ul>
             </nav>
         </header>
